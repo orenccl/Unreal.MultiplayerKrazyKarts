b67ae75d1050ffb4fe273e3ccdef65267e0b7490
Jun <oren.ccl@gmail.com>
2023-03-08 13:16:41 +0800

    [!] Init

---
+++ Source/KrazyKarts/KrazyKarts.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KrazyKarts.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KrazyKarts, "KrazyKarts" );
---
+++ Source/KrazyKarts/KrazyKartsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KrazyKartsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KRAZYKARTS_API AKrazyKartsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
